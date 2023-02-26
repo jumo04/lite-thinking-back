da31a64562bb23569bdde8d240c076ac4aee7b8d
Juan Fernando Moreno <juanfernandomoreno04@gmail.com>
2023-02-26 18:24:48 -0500

    adding routes url for cors local host and amazon front end

--- server.js
+++ server.js
@@ -29,22 +29,14 @@ db.mongoose
     process.exit();
   });
 
-var corsOptions = {
-  origin: ["http://localhost:8081", "https://main.d2d5zxiqav1vu1.amplifyapp.com/"]
-};
+// var corsOptions = {
+//   origin: ["http://localhost:8081", "https://main.d2d5zxiqav1vu1.amplifyapp.com/"]
+// };
 
 
 var allowedOrigins = ['http://localhost:8081',
-                      'https://main.d2d5zxiqav1vu1.amplifyapp.com/'];app.use(cors({
-  origin: function(origin, callback){    // allow requests with no origin 
-    // (like mobile apps or curl requests)
-    if(!origin) return callback(null, true);    if(allowedOrigins.indexOf(origin) === -1){
-      var msg = 'The CORS policy for this site does not ' +
-                'allow access from the specified Origin.';
-      return callback(new Error(msg), false);
-    }    return callback(null, true);
-  }
-}));
+                      'https://main.d2d5zxiqav1vu1.amplifyapp.com'];
+
 
 app.use(cors());
 
