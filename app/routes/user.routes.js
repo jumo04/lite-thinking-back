e3ef8e75372f659bea5a50a9b0419267f9010a23
Juan Fernando Moreno <juanfernandomoreno04@gmail.com>
2023-02-26 22:35:42 -0500

    access control

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -5,6 +5,7 @@ const controllerEnterprise = require("../controllers/enterprise.controller");
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "https://54.237.60.164.nip.io"); 
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
