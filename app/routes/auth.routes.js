40251a2fb90e9d46b2c209a922ec372e4f026c08
Juan Fernando Moreno <juanfernandomoreno04@gmail.com>
2023-02-26 23:12:20 -0500

    access control

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -12,7 +12,6 @@ app.use(express.urlencoded({ extended: true }));
 
 module.exports = function(app) {
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://main.d2d5zxiqav1vu1.amplifyapp.com"); 
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
